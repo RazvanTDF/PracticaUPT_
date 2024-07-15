8fa59fc9f0e178a8cbf71ea61a7263eb12d2f3a8
RazvanTDF <razvantdf@gmail.com>
2024-07-15 21:08:41 +0300

    create api c# asp

---
+++ UserAPI/Models/user.cs
@@ -0,0 +1,10 @@
+namespace UserAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
+//classic C# class with properties for Id, Name, and Email
---
+++ UserAPI/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using UserAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:5068")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors(); 
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
